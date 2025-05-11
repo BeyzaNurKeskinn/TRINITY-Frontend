9e5cb61a0f5ada4940d225e0b87f420e53d25546
BeyzaNurKeskinn <beyzanurhorasan89@gmail.com>
2025-05-11 15:02:39 +0300

    Initial commit: Set up Vite + React + TypeScript project with Tailwind CSS and created a basic landing page with 'Trinity Şifre Yöneticisi' title.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600">Trinity Şifre Yöneticisi</h1>
+    </div>
+    </>
+  )
+}
+ 
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // TypeScript ve React dosyalarını kapsar
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
